404a9d2b020a784cccd4893beb118c39f88a2972
chigozieokoroafor <okoroaforc14@gmail.com>
2023-12-12 23:34:54 +0100

    deploying to render

--- routes/transfers.js
+++ routes/transfers.js
@@ -4,8 +4,18 @@ const { validateToken } = require("../config/functions");
 const transfers_r = express.Router();
 transfers_r.use(express.json());
 
-transfer_r.get("/verifyreciepient", validateToken, async(req, res)=>{
+transfers_r.get("/verifyreciepient", validateToken, async(req, res)=>{
     const user = req.user;
+    try{
+
+        const {_id} = user;
+
+    }catch(error){
+
+    }
+})
+
+transfers_r.post("/makeTransfer", validateToken, async(req, res)=>{
     
 })
 
